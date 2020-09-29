1d7817effb99568b6e8e5f7e6c888004ddbe81b6
marinakosova <marina.kosova@gmail.com>
2020-09-29 18:49:45 -0500

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let total = 0;
+    if (Array.isArray(array) && array.length) {
+        array.forEach(element => {
+            total += element;
+        });
+    } else {
+        return 0;
+    }
+    return (total / array.length);
 }
